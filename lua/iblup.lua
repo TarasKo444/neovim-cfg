2eea572f1da0bb5a2b6754417e89672c737b94a8
iblup <tarkorytko@gmail.com>
2025-07-01 16:49:30 +0100

    better copy/paste

--- lua/iblup.lua
+++ lua/iblup.lua
@@ -32,6 +32,8 @@ local opts = { noremap = true, silent = true }
 
 vim.keymap.set({"n", "v"}, "<leader>y", '"+y', opts)
 vim.keymap.set({"n", "v"}, "<leader>p", '"+p', opts)
+vim.keymap.set({"n", "v"}, "<leader>Y", '"+Y', opts)
+vim.keymap.set({"n", "v"}, "<leader>P", '"+P', opts)
 
 vim.keymap.set("v", "<", "<gv", opts)
 vim.keymap.set("v", ">", ">gv", opts)
