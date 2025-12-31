2c537b58dd5f33e425f499a8a1b6a88fed6c5ea5
iblup <tarkorytko@gmail.com>
2025-12-31 02:27:11 +0000

    treesitter main branch update

--- lua/plugins_notvscode/treesitter.lua
+++ lua/plugins_notvscode/treesitter.lua
@@ -1,25 +1,88 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  build = ":TSUpdate",
-  event = { "BufReadPost", "BufNewFile" },
-  config = function()
-    local configs = require("nvim-treesitter.configs")
-    configs.setup({
-      ensure_installed = {
-        "c", "cpp", "lua", "vim", "vimdoc", "javascript", "html", "python", "rust"
-      },
-      modules = {},
-      ignore_install = {},
-      auto_install = false,
-      sync_install = false,
-      highlight = {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    branch = "main",
+    build = ":TSUpdate",
+    lazy = false,
+    dependencies = {
+      { "nvim-treesitter/nvim-treesitter-textobjects", branch = "main" },
+      "nvim-treesitter/nvim-treesitter-context",
+    },
+    config = function()
+      local ts = require("nvim-treesitter")
+
+      local ensure_installed = { "c", "cpp", "lua", "vim", "vimdoc", "javascript", "html", "python", "rust" }
+
+      local already_installed = ts.get_installed()
+
+      local to_install = vim
+        .iter(ensure_installed)
+        :filter(function(parser)
+          return not vim.tbl_contains(already_installed, parser)
+        end)
+        :totable()
+
+      if #to_install > 0 then
+        ts.install(to_install)
+      end
+
+      vim.api.nvim_create_autocmd("FileType", {
+        group = vim.api.nvim_create_augroup("EnableTreesitterHighlighting", { clear = true }),
+        desc = "Try to enable tree-sitter syntax highlighting",
+        pattern = "*", -- run on *all* filetypes
+        callback = function()
+          pcall(function()
+            vim.treesitter.start()
+          end)
+        end,
+      })
+
+      require("treesitter-context").setup({
         enable = true,
-        additional_vim_regex_highlighting = false,
-      },
-      indent = { enable = true },
-      incremental_selection = {
+        max_lines = 3,
+        multiwindow = false,
+        min_window_height = 0,
+        line_numbers = true,
+        multiline_threshold = 20,
+        trim_scope = "outer",
+        mode = "cursor",
+      })
+    end,
+  },
+  {
+    "nvim-treesitter/nvim-treesitter-context",
+    event = "BufRead",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      event = "BufRead",
+    },
+    opts = {
+      multiwindow = true,
+    },
+  },
+  {
+    "nvim-treesitter/nvim-treesitter-textobjects",
+    branch = "main",
+    keys = function()
+      local select = function(query, query_group)
+        return function()
+          require("nvim-treesitter-textobjects.select").select_textobject(query, query_group or "textobjects")
+        end
+      end
+
+      return {
+        { "af", select("@function.outer"), mode = { "x", "o" }, desc = "Select outer function" },
+        { "if", select("@function.inner"), mode = { "x", "o" }, desc = "Select inner function" },
+        { "ac", select("@class.outer"), mode = { "x", "o" }, desc = "Select outer class" },
+        { "ic", select("@class.inner"), mode = { "x", "o" }, desc = "Select inner class" },
+        { "as", select("@local.scope", "locals"), mode = { "x", "o" }, desc = "Select local scope" },
+      }
+    end,
+    opts = {
+      select = {
         enable = true,
+        lookahead = true,
       },
-    })
-  end
+    },
+  },
 }
