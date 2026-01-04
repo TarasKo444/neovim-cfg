12db796c8838bcef7018dc220569e7e819da011a
iblup <tarkorytko@gmail.com>
2026-01-04 01:12:53 +0000

    copilot keymap

--- lua/plugins_notvscode/copilot.lua
+++ lua/plugins_notvscode/copilot.lua
@@ -1,3 +1,13 @@
 return {
-  'github/copilot.vim'
+  'github/copilot.vim',
+  init = function()
+    vim.g.copilot_no_tab_map = true
+  end,
+  config = function()
+    vim.keymap.set('i', '<C-i>', 'copilot#Accept("\\<CR>")', {
+      expr = true,
+      replace_keycodes = false
+    })
+    vim.g.copilot_no_tab_map = true
+  end
 }
